e04c34dd24fd43739be127071e930c153c10a6c1
chandrakumarreddy <chandrapenugonda655@gmail.com>
2021-10-24 14:50:16 +0530

    use compression middleware

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import http from 'http';
-import path from 'path';
 import express from 'express';
 import reload from 'reload';
-
+import compression from 'compression';
 // import expressLayouts from 'express-ejs-layouts';
+import shouldCompress from './utils/shouldCompress';
 
 dotenv.config({ path: path.join(path.resolve(''), '.env.local') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.disable('x-powered-by');
 app.set('view engine', 'ejs');
 app.set('views', path.join(path.resolve('src'), 'resources/views'));
 // app.use(expressLayouts);
+app.use(compression({ filter: shouldCompress }));
 
 app.get('/', (req, res) => {
   res.render('home');
