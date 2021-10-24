a91715c81b88190cf23d126c2ac59e38592b4809
chandrakumarreddy <chandrapenugonda655@gmail.com>
2021-10-24 14:30:48 +0530

    fix reload issue

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+import http from 'http';
 import path from 'path';
 import express from 'express';
+import reload from 'reload';
+
 // import expressLayouts from 'express-ejs-layouts';
+
+dotenv.config({ path: path.join(path.resolve(''), '.env.local') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +19,12 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
   console.log(`server is listening at PORT ${PORT}`);
 });
+
+if (process.env.NODE_ENV === 'development') {
+  reload(app);
+}
